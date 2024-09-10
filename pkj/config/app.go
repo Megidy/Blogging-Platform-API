5a7757a79bc44f3d2cee8ff1f40e0804608a8e44
Megidy <byrka.sergiy@gmail.com>
2024-09-10 18:41:52 +0300

    Update app.go

--- pkj/config/app.go
+++ pkj/config/app.go
@@ -10,7 +10,7 @@ import (
 var db *gorm.DB
 
 func Connect() {
-	dsn := "root:2231@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:test@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
 	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
